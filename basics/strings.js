2ebd861d2d8ab7010e048f8a0a5d6a884a1c774e
Kyle <kylespresser@gmail.com>
2018-04-14 10:50:09 -0400

    lecture 10

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+// let firstName = 'Kyle'
+// let lastName = 'Spresser'
+// let fullName = firstName + ' ' + lastName
+
+// console.log(fullName);
+
+
+let city = 'Lakeland'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log(location)
