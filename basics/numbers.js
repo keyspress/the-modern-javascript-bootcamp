603cdcf04f845135c3041c3d68a67fe3e806aede
Kyle <kylespresser@gmail.com>
2018-04-14 11:04:18 -0400

    lecture 11

---
+++ basics/numbers.js
@@ -0,0 +1,16 @@
+// let x = 11
+
+// let num = (x + 1) * 2
+
+// console.log(num)
+
+// let age = 41
+// let dogYears = (age + 1) / 7
+
+// console.log(dogYears)
+
+let studentScore = 19
+let maxScore = 20
+let percent = studentScore / maxScore * 100
+
+console.log(percent)
