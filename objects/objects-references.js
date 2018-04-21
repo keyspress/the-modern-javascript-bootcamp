9d05ebd9378578ccbe98d385676df5cd1f6e2dcc
Kyle <kylespresser@gmail.com>
2018-04-21 13:23:31 -0400

    lecture 32

---
+++ objects/objects-references.js
@@ -0,0 +1,31 @@
+let myAccount = {
+    name: 'Drew Deed',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function(account, amount) {
+    account.expenses = account.expenses + amount;
+}
+
+let addIncome = function(account, amount) {
+    account.income = account.income + amount;
+}
+
+let resetAccount = function(account) {
+    account.expenses = 0;
+    account.income = 0;
+}
+
+let getAccountSummary = function(account) {
+    let balance = account.income - account.expenses;
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
+}
+
+
+addExpense(myAccount, 2.50);
+addIncome(myAccount, 3.50);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
+// console.log(myAccount);
