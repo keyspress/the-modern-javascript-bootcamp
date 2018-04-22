db76f160cb3d9e8fda5828fa1675fa887002b3e1
Kyle Desktop <kylespresser@gmail.com>
2018-04-21 22:19:15 -0400

    lecture 37

---
+++ arrays/todo.js
@@ -0,0 +1,7 @@
+const todos = ['Get milk', 'Drink milk', 'Cry over having no milk']
+
+console.log(`You have ${todos.length} things to do.`)
+
+console.log(`Todo: ${todos[0]}`)
+
+console.log(`Todo: ${todos[todos.length - 2]}`)
