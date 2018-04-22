d327f08485dcca32b7ee986bf18a87cc520d2db7
Kyle Desktop <kylespresser@gmail.com>
2018-04-21 22:48:49 -0400

    lecture 39

--- arrays/notes.js
+++ arrays/notes.js
@@ -10,6 +10,11 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 notes[2] = 'tree tree tree'
 
+notes.forEach(function(item, index){
+  console.log(index)
+  console.log(item)
+})
+
 console.log(notes.length)
 
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,8 +1,10 @@
 const todos = ['Get milk', 'Drink milk', 'Cry over having no milk', 'Deny wanting milk in the first place', 'Get more milk']
 
-todos.splice(2, 1)
-todos.push('Wonder if you would be happier with cheese')
-todos.shift()
+// todos.splice(2, 1)
+// todos.push('Wonder if you would be happier with cheese')
+// todos.shift()
 
 console.log(`You have ${todos.length} things to do.`)
-console.log(todos)
+todos.forEach(function(item, index) {
+  console.log(`${index + 1}. ${item}`)
+})
