fecbb5fa5d5c905f29ab830308ff76d0a4d0b84d
Kyle Desktop <kylespresser@gmail.com>
2018-04-21 22:39:57 -0400

    lecture 38

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,5 +1,15 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// console.log(notes.pop())
+// notes.push('My new note')
+
+// console.log(notes.shift())
+// notes.unshift('The first note ever!')
+
+// notes.splice(1, 1, "Little girl you're in the middle")
+
+notes[2] = 'tree tree tree'
+
 console.log(notes.length)
 
-console.log(notes[notes.length - 1])
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,7 +1,8 @@
-const todos = ['Get milk', 'Drink milk', 'Cry over having no milk']
+const todos = ['Get milk', 'Drink milk', 'Cry over having no milk', 'Deny wanting milk in the first place', 'Get more milk']
+
+todos.splice(2, 1)
+todos.push('Wonder if you would be happier with cheese')
+todos.shift()
 
 console.log(`You have ${todos.length} things to do.`)
-
-console.log(`Todo: ${todos[0]}`)
-
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(todos)
