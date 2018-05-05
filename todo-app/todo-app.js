40dae0b6084f8e29ea7da255184b7fe4fe23e405
Kyle <kylespresser@gmail.com>
2018-05-05 13:25:22 -0400

    lecture 58 challenge complete

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -43,15 +43,22 @@ const renderTodos = function(todos, filters) {
 
 renderTodos(todos, filters)
 
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('You clicked me! Yay!')
-})
+// document.querySelector('#add-todo').addEventListener('click', function (e) {
+//     console.log('You clicked me! Yay!')
+// })
 
-document.querySelector('#new-todo-text').addEventListener('input', function(e) {
-    console.log(e.target.value)
-})
+// document.querySelector('#new-todo-text').addEventListener('input', function(e) {
+//     console.log(e.target.value)
+// })
 
 document.querySelector('#search-todos').addEventListener('input', function(e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
+})
+
+document.querySelector('#add-todo').addEventListener('submit', function(e) {
+    e.preventDefault()
+    todos.push({ body: e.target.elements.newTodo.value, completed: false })
+    renderTodos(todos, filters)
+    e.target.elements.newTodo.value = ''
 })
