60d9bcaef7fbd7384ee7028659b0140d9d6ead41
Kyle <kylespresser@gmail.com>
2018-05-05 10:12:47 -0400

    lecture 52 challenge complete

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -5,6 +5,17 @@
 // p.remove()
 
 //Query all and remove
+const notes = [{
+    title: 'My next trip',
+    body: 'I want to go to Jupiter'
+}, {
+    title: 'Habbits to work on',
+    body: 'Code more'
+}, {
+    title: 'Office modifications',
+    body: 'Get an office'
+}]
+
 const ps = document.querySelectorAll('p')
 
 ps.forEach(function(p) {
@@ -13,3 +24,6 @@ ps.forEach(function(p) {
     p.textContent = "Don't look at this!!!"
 })
 
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This a new element from JS'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,34 @@
+const todos = [{
+    body: 'Get milk',
+    completed: true
+}, {
+    body: 'Drink milk',
+    completed: true
+}, {
+    body: 'Cry over having no milk',
+    completed: true
+}, {
+    body: 'Deny wanting milk in the first place',
+    completed: false
+}, {
+    body: 'Get more milk',
+    completed: false
+}]
 
-const ps = document.querySelectorAll('p')
+const summary = document.createElement('p')
+summary.textContent = `You have ${todos.length} todos left.`
+document.querySelector('body').appendChild(summary)
 
-ps.forEach(function(p) {
-    if(p.textContent.includes('the')) {
-        p.remove()
-    }
+todos.forEach(function(item) {
+    const todo = document.createElement('p')
+    todo.textContent = item.body
+    document.querySelector('body').appendChild(todo)
 })
+
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function(p) {
+//     if(p.textContent.includes('the')) {
+//         p.remove()
+//     }
+// })
