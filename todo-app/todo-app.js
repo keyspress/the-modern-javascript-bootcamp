e665395c64966eb9e9291c207550ccdbf82effa0
Kyle <kylespresser@gmail.com>
2018-05-05 12:10:34 -0400

    lecture 57 challenge complete

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,20 +15,33 @@ const todos = [{
     completed: false
 }]
 
+const filters = {
+    searchText: ''
+}
 
-const completeTodos = todos.filter(function(todo) {
-    return !todo.completed
-})
+const renderTodos = function(todos, filters) {
+    const incompleteTodos = todos.filter(function (todo) {
+        return !todo.completed
+    })
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${completeTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+    const filteredTodos = incompleteTodos.filter(function (todo) {
+        return todo.body.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-todos.forEach(function (item) {
-    const todo = document.createElement('p')
-    todo.textContent = item.body
-    document.querySelector('body').appendChild(todo)
-})
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${filteredTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (item) {
+        const todo = document.createElement('p')
+        todo.textContent = item.body
+        document.querySelector('#todos').appendChild(todo)
+    })
+}
+
+renderTodos(todos, filters)
 
 document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('You clicked me! Yay!')
@@ -36,4 +49,9 @@ document.querySelector('#add-todo').addEventListener('click', function (e) {
 
 document.querySelector('#new-todo-text').addEventListener('input', function(e) {
     console.log(e.target.value)
+})
+
+document.querySelector('#search-todos').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
