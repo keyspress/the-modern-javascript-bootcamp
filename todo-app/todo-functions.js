beb2dce6128502da12c4875e4cae436dbe88f054
Kyle <kylespresser@gmail.com>
2018-05-19 13:10:35 -0400

    lecture 70, saving before pulling teacher code. variable names becoming unmanageable to follow lectures

--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -45,6 +45,16 @@ const removeTodo = function (id) {
 	}
 }
 
+const toggleTodo = function (id) {
+	const todo = todos.find(function(todo) {
+		return todo.id === id
+	})
+
+	if(todo !== undefined) {
+		todo.completed = !todo.completed
+	}
+}
+
 // Get the DOM elements for an individual todo
 const generateTodoDOM = function (item) {
 	const todoEl = document.createElement('div')
@@ -54,7 +64,13 @@ const generateTodoDOM = function (item) {
 
 	// Setup checkbox
 	checkbox.setAttribute('type', 'checkbox')
+	checkbox.checked = item.completed
 	todoEl.appendChild(checkbox)
+	checkbox.addEventListener('change', function() {
+		toggleTodo(todos.id)
+		saveTodos(todos)
+		renderTodos(todos, filters)
+	})
 
 	// Setup Todo text 
 	todoText.textContent = item.body
